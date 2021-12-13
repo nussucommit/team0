158b33877988672c6ba7583d9761244940abb75e
DiiSollertia <xz.khoo.work@gmail.com>
2021-12-13 17:21:14 +0800

    Applied flatter RDB design

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Submission, Post, Comment
+from .models import User, Post, Comment
 
 admin.site.register(User)
-admin.site.register(Submission)
 admin.site.register(Post)
 admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -2,26 +2,41 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-	user = models.CharField(primary_key=True, max_length=64)
+	id = models.CharField(primary_key=True, max_length=64)
 	password = models.CharField(max_length=64)
 
 	def __str__(self):
 		return self.user
 
+'''
+#Remove inheritence and opt for flatter RDB design
 class Submission(models.Model):
+	## Use automatic PK for submission_id
 	content = models.TextField()
 	datetime = models.DateTimeField(auto_now=True)
 	likes = models.IntegerField()
+'''
 
-class Post(Submission):
-	## Use automatic PK for PostId
+class Post(models.Model):
+	#post = models.IntegerField(primary_key=True)
+	#submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length=128)
+	content = models.TextField()
+	datetime = models.DateTimeField(auto_now=True)
+	likes = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.title
 
-class Comment(Submission):
-	## Use automatic PK for CommentId
+class Comment(models.Model):
+	#comment = models.IntegerField(primary_key=True)
+	#submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
 	post = models.ForeignKey(Post, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	content = models.TextField()
+	datetime = models.DateTimeField(auto_now=True)
+	likes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.content
