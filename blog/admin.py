5bbd025e7dbdaf90438e080214837c0af7b66f5a
DiiSollertia <xz.khoo.work@gmail.com>
2021-12-15 10:47:52 +0800

    admin console changes to fit custom user model

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 from .models import User, Post, Comment
 
-admin.site.register(User)
+class Admin(UserAdmin):
+	list_display = ('username', 'date_joined', 'last_login', 'is_admin', 'is_staff')
+	search_fields = ('username',)
+	read_only = ('date_joined', 'last_login')
+
+	filter_horizontal = ()
+	list_filter = ()
+	fieldsets = ()
+
+admin.site.register(User, Admin)
 admin.site.register(Post)
 admin.site.register(Comment)
