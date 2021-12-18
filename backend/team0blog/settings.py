5bc4589444ddb07ecf2eeba35684649ce7ca2305
Anshumaan Tyagi <75516251+anshumaantgi@users.noreply.github.com>
2021-12-18 10:58:28 +0800

    Update settings.py
    Added third party apps for authorization.

--- backend/team0blog/settings.py
+++ backend/team0blog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #Third-Party Apps
+    'rest_framework',
+    'rest_framework.authtoken',
 
     #my apps
     'blog',
