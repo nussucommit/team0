24806b75cc6e3472a2f01cd0ceb09bc9ee2666e5
DiiSollertia <xz.khoo.work@gmail.com>
2022-01-05 08:07:19 +0800

    fixed typos

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from rest_framework_simplejwt import views as jwt_views
+#from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('results', views.results.as_view(), name='show_posts'),
